1942e5b144d4b994d74ff9f0b8623852bf0845f0
Aidan Feay <aidan.feay@voxmedia.com>
2016-07-11 12:29:46 -0500

    jquery and canvas

--- assets/javascripts/editor.js
+++ assets/javascripts/editor.js
@@ -1,3 +1,11 @@
 (function() {
-	console.log('editor loaded');
+  var loadCanvas = function() {
+    var c = document.getElementById("tonr-canvas");
+    var ctx = c.getContext("2d");
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0,0,200,200);
+  }
+  $(document).ready(function(){
+    loadCanvas();
+  });
 })();
