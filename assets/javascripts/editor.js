10afecb02bd853670fbd69fff88e16299a7e3075
Aidan Feay <aidan.feay@voxmedia.com>
2016-07-12 10:09:17 -0500

    include center/scale solution

--- assets/javascripts/editor.js
+++ assets/javascripts/editor.js
@@ -1,6 +1,61 @@
 (function() {
   var c,
       ctx;
+
+  /**
+   * By Ken Fyrstenberg Nilsen
+   * drawImageProp(context, image [, x, y, width, height [,offsetX, offsetY]])
+   * If image and context are only arguments rectangle will equal canvas
+   * See http://stackoverflow.com/questions/21961839/simulation-background-size-cover-in-canvas
+  */
+  var drawImageProp = function(ctx, img, x, y, w, h, offsetX, offsetY) {
+
+      if (arguments.length === 2) {
+          x = y = 0;
+          w = ctx.canvas.width;
+          h = ctx.canvas.height;
+      }
+
+      // default offset is center
+      offsetX = typeof offsetX === "number" ? offsetX : 0.5;
+      offsetY = typeof offsetY === "number" ? offsetY : 0.5;
+
+      // keep bounds [0.0, 1.0]
+      if (offsetX < 0) offsetX = 0;
+      if (offsetY < 0) offsetY = 0;
+      if (offsetX > 1) offsetX = 1;
+      if (offsetY > 1) offsetY = 1;
+
+      var iw = img.width,
+          ih = img.height,
+          r = Math.min(w / iw, h / ih),
+          nw = iw * r,   // new prop. width
+          nh = ih * r,   // new prop. height
+          cx, cy, cw, ch, ar = 1;
+
+      // decide which gap to fill    
+      if (nw < w) ar = w / nw;                             
+      if (Math.abs(ar - 1) < 1e-14 && nh < h) ar = h / nh;  // updated
+      nw *= ar;
+      nh *= ar;
+
+      // calc source rectangle
+      cw = iw / (nw / w);
+      ch = ih / (nh / h);
+
+      cx = (iw - cw) * offsetX;
+      cy = (ih - ch) * offsetY;
+
+      // make sure source rectangle is valid
+      if (cx < 0) cx = 0;
+      if (cy < 0) cy = 0;
+      if (cw > iw) cw = iw;
+      if (ch > ih) ch = ih;
+
+      // fill image in dest. rectangle
+      ctx.drawImage(img, cx, cy, cw, ch,  x, y, w, h);
+  };
+
   var loadCanvas = function() {
     c = document.getElementById("canvas");
     ctx = c.getContext("2d");
@@ -10,14 +65,15 @@
 
   var loadImage = function() {
     var img = document.getElementById("uploaded-img");
-    $("#canvas").attr('width',img.width);
-    $("#canvas").attr('height',img.height);
+    // $("#canvas").attr('width',img.width);
+    // $("#canvas").attr('height',img.height);
  
     var canvasHeight = $("#canvas").height;
     var canvasWidth = $("#canvas").width;
 
-    ctx.clearRect(0,0,c.width, canvasHeight);
-    ctx.drawImage(img, 0,0, img.width, img.height, c.width, canvasHeight);
+    ctx.clearRect(0,0, canvasWidth, canvasHeight);
+    ///ctx.drawImage(img, 0,0, img.width, img.height, canvasWidth, canvasHeight);
+    drawImageProp(ctx, img);
   };
 
   var loadUploader = function() {
