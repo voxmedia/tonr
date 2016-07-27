612289bcf65fe78e145979d06380425b017f6235
pam- <pamela.assogba@voxmedia.com>
2016-07-26 21:45:35 -0400

    asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Tonr
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.compile = true
+    config.serve_static_files = true
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
