3bc993b40ca8aefd1324b39aa2ac36e32169db5d
pam- <pamela.assogba@voxmedia.com>
2016-07-26 21:07:54 -0400

    removing s3 stuff

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -1,5 +1,5 @@
 class EditorController < ApplicationController
-  before_action :set_s3_direct_post, only: [:home]
+  # before_action :set_s3_direct_post, only: [:home]
 
   def home
     @picture = Picture.new
@@ -8,7 +8,7 @@ class EditorController < ApplicationController
 
   private
 
-  def set_s3_direct_post
-    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
-  end
+  # def set_s3_direct_post
+  #   @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+  # end
 end
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,5 @@
 class PicturesController < ApplicationController
-  before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
+  # before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
   before_action :set_user
 
   def new
@@ -18,7 +18,7 @@ class PicturesController < ApplicationController
     @user = User.find(session[:user_id])
   end
 
-  def set_s3_direct_post
-    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
-  end
+  # def set_s3_direct_post
+  #   @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+  # end
 end
